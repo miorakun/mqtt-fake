375b9f96c2884ec260c4542e19ef4568123810c0
simple-geek <simplegeek@localhost.localdomain>
2022-03-31 18:42:34 +0430

    add void send signal for sig and duration entry

--- mqtt-fake.cpp
+++ mqtt-fake.cpp
@@ -5,3 +5,23 @@ extern "C" {
 }
 #include <AsyncMqttClient.h>
 #include <credentials.h>
+
+
+#define MQTT_HOST IPAddress(192,168,1,112)
+#define MQTT_PORT 1883
+#define SHORT_SPACE 250
+#define SHORT_SIG 350
+#define BETW 16000
+#define SEND_PIN 4
+
+String extendCommand = "LGSPSPSPSPSPSPSPLGSPLGLGLGSPLGSPSPSN";
+String reteractCommand = "LGSPSPSPSPSPSPSPLGSPLGLGLGSPLGSPLGSN";
+
+String manCommand = "LGSPSPSPSPSPSPSPLGSPLGLGLGLGLN";
+String autoCommand = "LGSPSPSPSPSPSPSPLGSPLGLGSPLGLGLGLGSN";
+
+void sendSig(String sig) {
+    if (duration < 1)duration =1 ;
+    if (duration > 90)duration = 90;
+    unsigned long entry = millis();
+}
